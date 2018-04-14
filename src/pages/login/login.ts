6524456b435fb7b27ff2c6569ca6cc03533417cd
linxianying <soullxy@qq.com>
2018-04-14 12:59:39 +0800

    login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { Student } from '../../entities/student';
+
+import { RegisterPage } from '../register/register';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
@@ -9,13 +13,14 @@ import { NavController, NavParams } from 'ionic-angular';
 
 export class LoginPage {
   fromPage: string;
+  student = {} as Student;  // an objecct as User
 	
 	
 	
-	constructor(public navCtrl: NavController, public navParams: NavParams) 
-	{
-		this.fromPage = navParams.get('fromPage');
-	}
+  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  {
+	this.fromPage = navParams.get('fromPage');
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
