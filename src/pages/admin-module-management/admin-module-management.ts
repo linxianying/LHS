233f515925900fc2361b72e669f84de27a0a214e
blablamango <32839438+blablamango@users.noreply.github.com>
2018-04-14 19:07:30 +0800

    admin module management page

--- src/pages/admin-module-management/admin-module-management.ts
+++ src/pages/admin-module-management/admin-module-management.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { AdminProvider } from '../../providers/administrator/administrator';
+
+import { Module } from '../../entities/module';
+
+
 /**
  * Generated class for the AdminModuleManagementPage page.
  *
@@ -14,11 +19,71 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class AdminModuleManagementPage {
 
+	errorMessage: string;
+	infoMessage: string;
+	newModule: Module;
+	modules: List<Module>;
+	moduleToUpdate: Module;
+	moduleToAssign: Module;
+
+	submitted: boolean;
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	this.newModule = new Module();
+  	this.modules = new List<>();
+  	this.moduleToUpdate = new Module();
+  	this.moduleToAssign = new Module();
+  	this.submitted = false;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdminModuleManagementPage');
   }
 
+  clear()
+  {
+  		this.infoMessage = null;
+		this.errorMessage = null;
+		this.submitted = false;
+		this.newModule = new Module();
+  }
+
+  create(createModuleForm: NgForm)
+	{		
+		this.submitted = true;
+		
+		this.infoMessage = null;
+		this.errorMessage = null;
+		
+		if (createModuleForm.valid) 
+		{		
+			this.adminProvider.createModule(this.newModule).subscribe(
+				response => {					
+					this.infoMessage = "New module " + response.Id + " created successfully";
+				},
+				error => {
+					this.errorMessage = "HTTP " + error.status + ": " + error.error.message;
+				}
+			);
+		}
+	}
+
+	update(updateModuleForm: NgForm)
+	{		
+		this.submitted = true;
+		
+		if (updateModuleForm.valid) 
+		{		
+			this.adminProvider.updateModule(this.moduleToUpdate).subscribe(
+				response => {					
+					this.infoMessage = "Module updated successfully";
+				},
+				error => {
+					this.errorMessage = "HTTP " + error.status + ": " + error.error.message;
+				}
+			);
+		}
+		
+	}
 }
