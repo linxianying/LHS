39337b1517ba3d6f2048354b8f9ab6a386883104
linxianying <soullxy@qq.com>
2018-04-14 13:58:53 +0800

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+import { AlertController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 import { Student } from '../../entities/student';
 
 import { RegisterPage } from '../register/register';
+
+import { StudentProvider } from '../../providers/student/student';
 
 @Component({
   selector: 'page-login',
@@ -13,17 +18,80 @@ import { RegisterPage } from '../register/register';
 
 export class LoginPage {
   fromPage: string;
-  student = {} as Student;  // an objecct as User
+  student = {} as Student;
+  submitted: boolean;
+  isLogin: boolean;
+  username: string;
+  password: string;
 	
 	
-	
-  constructor(public navCtrl: NavController, public navParams: NavParams) 
+	constructor(public navCtrl: NavController,
+        public alertCtrl: AlertController,
+        public toastCtrl: ToastController,
+        public navParams: NavParams,
+        public studentProvider: StudentProvider)
   {
-	this.fromPage = navParams.get('fromPage');
+    this.submitted = false;
+    this.isLogin = false;
+    this.fromPage = navParams.get('fromPage');
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+    if(sessionStorage.getItem("isLogin") === "true")
+    {
+      this.isLogin = true;
+    }
+    
+    this.username = sessionStorage.getItem("username");
+  }
+
+  clear()
+  {
+    this.username = "";
+    this.password = "";
+  }
+
+  login(loginForm: NgForm)
+  {
+    this.submitted = true;
+    
+    if (loginForm.valid) 
+    {
+      if((this.username == "student") && (this.password == "password"))
+      {       
+        
+        this.isLogin = true;
+        
+        sessionStorage.setItem("username", this.username);   
+        sessionStorage.setItem("isLogin", "true");
+        
+        this.studentProvider.setLoginCredential(this.username, this.password);
+        
+        let toast = this.toastCtrl.create(
+        {
+          message: 'Welcome back ' + this.username,
+          duration: 3000
+        });
+        
+        toast.present();
+      }
+      else
+      {
+        let alert = this.alertCtrl.create(
+        {
+          title: 'Login',
+          subTitle: 'Invalid login credential',
+          buttons: ['OK']
+        });
+        
+        alert.present();      
+      }
+    }
+    else
+    {
+    }
   }
 
 }
--- src/providers/student/student.ts
+++ src/providers/student/student.ts
@@ -1,17 +1,55 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { Platform } from 'ionic-angular';
 
-/*
-  Generated class for the StudentProvider provider.
+import { Student } from '../../entities/student';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+const httpOptions = {
+	headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable()
 export class StudentProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello StudentProvider Provider');
-  }
+    ipAddress = '192.168.0.100';
+	portNo = '8080';
+	fullBaseUrl = 'http://' + this.ipAddress + ':' + this.portNo + '/LearningHubSystem-war/Resources/Student';
+	
+	baseUrl = "/api/Student";
+	
+	username = "";
+	password = "";
+	loginCredential = "";
+	
+	
+	
+	constructor(public platform: Platform,
+				private httpClient: HttpClient)
+	{
+	}
+
+	setLoginCredential(username: string, password: string)
+	{
+		this.username = username;
+		this.password = password;
+		this.loginCredential = "?username=" + username + "&password=" + password;
+	}
+
+	private handleError(error: HttpErrorResponse)
+	{
+		if (error.error instanceof ErrorEvent) 
+		{		
+			console.error('An unknown error has occurred:', error.error.message);
+		} 
+		else 
+		{		
+			console.error(" A HTTP error has occurred: " + `HTTP ${error.status}: ${error.error.message}`);
+		}
+		
+		return new ErrorObservable(error);
+	}
 
 }
