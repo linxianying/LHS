146886c466c1f4caab153932fe4d063a1130a208
Wang Yinhan <wangyinhan10000@gmail.com>
2018-04-17 20:35:50 +0800

    student details page

---
+++ src/pages/student-details/student-details.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ModalController, Platform, NavParams, ViewController, NavController } from 'ionic-angular';
+import { Student } from '../../entities/student';
+
+/**
+ * Generated class for the StudentDetailsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-student-details',
+  templateUrl: 'student-details.html',
+})
+
+
+  export class StudentDetailsPage {
+  student: Student;
+
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+    this.student = params.get('student');
+    
+    console.error('************ student 1: ' + this.student.name);
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+}
+
+
